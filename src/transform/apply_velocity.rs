049cd4497db6b46cdfaaaa61a172162318c5c1e7
XNTEABDSC <2364400855@qq.com>
2025-08-28 22:58:07 +0800

    e

--- src/transform/apply_velocity.rs
+++ src/transform/apply_velocity.rs
@@ -1,10 +1,10 @@
 use bevy_ecs::system::Query;
-use physics_basic::stats::{Agv, Dir, Pos, Vel};
+use physics_basic::stats::{Agv, DirVec, Pos, Vel};
 
 use crate::components::{change_component::ChangeComponent, stat_component::StatComponent};
 
 
-pub fn apply_velocity(mut query:Query<(&ChangeComponent<Pos>,&StatComponent<Vel>)>)
+pub fn apply_velocity<const DIM:usize>(mut query:Query<(&ChangeComponent<Pos<DIM>>,&StatComponent<Vel<DIM>>)>)
 {
     (&mut query).par_iter_mut().for_each(|(value,delta)|{
         value.add_change(Pos(delta.0.0));
