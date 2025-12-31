283ff9ee52a8a592062b87f9c5e2f99ce98ae994
XNTEABDSC <2364400855@qq.com>
2025-12-31 20:44:47 +0800

    .

--- src/utils/scope_trait.rs
+++ src/utils/scope_trait.rs
@@ -1,37 +1,46 @@
 use bevy::tasks::Scope as BevyScope;
-use wacky_bag::traits::scope::{ThreadScope, ThreadScopeCreator, ThreadScopeUser};
+use wacky_bag::traits::scope_no_ret::{ThreadScope, ThreadScopeCreator, ThreadScopeUser};
 
-pub struct ComputeTaskPoolScope<'scope, 'env: 'scope, T>(&'scope BevyScope<'scope, 'env, T>);
+pub struct ComputeTaskPoolScope<'scope, 'env: 'scope>(&'scope BevyScope<'scope, 'env, ()>);
 
-impl<'scope, 'env: 'scope, ScopeFuncOutput> wacky_bag::traits::scope::ThreadScope<'scope, ScopeFuncOutput>
-    for ComputeTaskPoolScope<'scope, 'env, ScopeFuncOutput>
-where
-    ScopeFuncOutput: Send,
+impl<'scope,'env:'scope> ThreadScope<'scope>
+	for ComputeTaskPoolScope<'scope, 'env>
 {
-    fn spawn<F>(&'scope self, f: F) -> ()
-    where
-        F: FnOnce() -> ScopeFuncOutput + std::marker::Send + 'scope,
-    {
-        self.0.spawn(async move { f() });
-    }
+
+	fn spawn<F>(&'scope self, f: F) -> ()
+	where
+		F: FnOnce() -> () + std::marker::Send + 'scope,
+	{
+		self.0.spawn(async move { f() });
+	}
+	
 }
+// impl<'scope, 'env: 'scope, ScopeFuncOutput> wacky_bag::traits::scope::ThreadScope<'scope, ScopeFuncOutput>
+//     for ComputeTaskPoolScope<'scope, 'env, ScopeFuncOutput>
+// where
+//     ScopeFuncOutput: Send,
+// {
+//     fn spawn<F>(&'scope self, f: F) -> ()
+//     where
+//         F: FnOnce() -> ScopeFuncOutput + std::marker::Send + 'scope,
+//     {
+//         self.0.spawn(async move { f() });
+//     }
+// }
 
 
 pub struct ComputeTaskPoolScopeCreator;
 
-impl<Output,ScopeFnOutput:'static+Send> ThreadScopeCreator<Output,ScopeFnOutput> for ComputeTaskPoolScopeCreator {
-    type Output <'env,F> = Vec< F::ScopeFnOutput>
-        where F: ThreadScopeUser<'env,Output =Output,ScopeFnOutput =ScopeFnOutput>;
-
-    fn scope<'env,F>(&mut self,f:F ) -> Self::Output<'env,F>
-        where F: ThreadScopeUser<'env,Output =Output,ScopeFnOutput =ScopeFnOutput>,
-             {
+impl ThreadScopeCreator for ComputeTaskPoolScopeCreator {
+    fn scope<'env,F>(&self,f:F ) -> ()
+        where F: ThreadScopeUser<'env>,
+    {
         bevy::tasks::ComputeTaskPool::get().scope(move |s:&BevyScope<'_,'env, _ >|{
             let ntscope=ComputeTaskPoolScope(s);
             // bbevy::tasks::TaskPool::scope did same
-            let extended_ntscope: &'env ComputeTaskPoolScope<'_, 'env, F::ScopeFnOutput> =
+            let extended_ntscope: &'env ComputeTaskPoolScope<'_, 'env> =
                 unsafe { std::mem::transmute(&ntscope) };
             f.use_scope(extended_ntscope);
-        })
+        });
     }
 }
