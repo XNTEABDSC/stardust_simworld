f9d1597030fa2a82c56ed25dc49b2b2fc982f768
XNTEABDSC <2364400855@qq.com>
2026-03-02 16:50:12 +0800

    gas resource

--- src/grid_gas/resource.rs
+++ src/grid_gas/resource.rs
@@ -1,9 +1,10 @@
 use std::{ops::Add, str};
 
-use bevy::ecs::resource::Resource;
-use frunk::{Func, HList, Poly, hlist::{self, HMappable, HZippable},};
-use statistic_physics::matters::{MattersBasic, MattersFull};
-use wacky_bag_bevy::{stat_component::{change::Change, stat::Stat}, utils::stat_for_hlist::{MapToChange, MapToStat}};
+use bevy::ecs::{resource::Resource, system::ResMut};
+use frunk::{Func, HList, Poly, ToMut, hlist::{self, HMappable, HZippable}};
+use statistic_physics::{formulas::calculate_matters_state, matters::{MattersBasic, MattersFull}};
+use wacky_bag::{structures::n_dim_array::t_n_dim_array::TNDimArray, utils::{select_zip::HSelectZippable, type_fn::{ChainFunc, ReverseFunc}}};
+use wacky_bag_bevy::{stat_component::{change::Change, stat::Stat}, utils::{stat_for_hlist::{HApplyChange, MapToChange, MapToStat}, thread_scope::ComputeTaskPoolScopeCreater}};
 
 
 use crate::grid::grid::GridResource;
@@ -23,3 +24,32 @@ pub struct GridGasResource<const DIM:usize>(pub GridResource<DIM,
 	>
 	>::Output
 >);
+
+pub fn grid_gas_apply_changes<const DIM:usize>(mut grid_gas:ResMut<GridGasResource<DIM>>){
+	grid_gas.0.for_each_mut_parallel(&|g,_|{
+		let (cs,r)
+			:(<<MattersBasic::<DIM> as HMappable<Poly<MapToChange>>>::Output as ToMut>::Output,_)
+			=g.to_mut().sculpt();
+		let (ss,r)
+			:(<<MattersBasic::<DIM> as HMappable<Poly<MapToStat>>>::Output as ToMut>::Output,_)
+			=r.sculpt();
+		cs.zip(ss).map(Poly(HApplyChange));
+		
+		calculate_matters_state(
+			g.to_mut()
+			.sculpt()
+			.0
+		);
+		//let (cs,r)
+		//	:(<MattersBasic::<DIM> as HMappable<Poly<MapToChange>>>::Output,_)
+		//	=g.sculpt();
+		//let to_add=cs.select_zip(
+		//	ChainFunc::<
+		//		ReverseFunc<MapToChange>,
+		//		MapToStat
+		//	>::default()
+		//	,r.sculpt().0
+		//);
+		
+	}, &ComputeTaskPoolScopeCreater);
+}
